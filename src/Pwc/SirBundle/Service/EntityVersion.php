17b71f04db62a62f10669a9e15f6683556aaf1d0
Jim Krezmien <jwmkrezmien@gmail.com>
2013-01-13 15:20:30 +0100

    Major refactoring of Logging component.

--- src/Pwc/SirBundle/Service/EntityVersion.php
+++ src/Pwc/SirBundle/Service/EntityVersion.php
@@ -32,59 +32,148 @@ class EntityVersion
         $this->username = $logEntry->getUsername();
         $this->modContextProvider = $modContextProvider;
 
-        $reversedMapping = $this->modContextProvider->getReversedEntityMapping();
-        $context = $this->modContextProvider->getContext();
+        //$context = $this->modContextProvider->getGlossary2();
 
+        foreach ($logEntry->getData() as $attribute => $value)
+        {
+            $this->modifications[] = new Modification(
+                                            $attribute,
+                                            $this->modContextProvider->hasGlossary(
+                                                $this->modContextProvider->getReversedEntityMapping($this->objectClass),
+                                                $attribute
+                                            ) ? $this->modContextProvider->getGlossary(
+                                                $this->modContextProvider->getReversedEntityMapping($this->objectClass),
+                                                $attribute,
+                                                $value['id']
+                                            ) : $value
+                                     );
+        }
+/*
         foreach ($logEntry->getData() as $subject => $value)
         {
             $this->modifications[] = new Modification($subject, array_key_exists($subject, $context[$this->getObjectName()]) && isset($context[$this->getObjectName()][$subject][$value['id']]) ? $context[$this->getObjectName()][$subject][$value['id']] : $value);
         }
-
+*/
     }
 
+    /**
+     * Get modifications set in the version update of this entity
+     *
+     * @return array
+     */
     public function getModifications()
     {
         return $this->modifications;
     }
 
+    /**
+     * Get a datetime object for the moment this entity version was set
+     *
+     * @return \DateTime
+     */
     public function getLoggedAt()
     {
         return $this->loggedAt;
     }
 
+    /**
+     * Get the action that was performed [create, update or delete]
+     *
+     * @return string
+     */
     public function getAction()
     {
         return $this->action;
     }
 
+    /**
+     * Get the namespace of the class
+     *
+     * @return string
+     */
     public function getObjectClass()
     {
         return $this->objectClass;
     }
 
+    /**
+     * Get the ID of the altered object
+     *
+     * @return integer
+     */
     public function getObjectId()
     {
         return $this->objectId;
     }
 
+    /**
+     * Get the version number
+     *
+     * @return integer
+     */
     public function getVersion()
     {
         return $this->version;
     }
 
+    /**
+     * Get the username of the one who made the alteration
+     *
+     * @return string
+     */
     public function getUsername()
     {
         return $this->version;
     }
 
+    /**
+     * Get the name of the object
+     *
+     * @return string
+     */
     public function getObjectName()
     {
-        $rm = $this->modContextProvider->getReversedEntityMapping();
-        return $rm[$this->objectClass];
+        return $this->modContextProvider->getReversedEntityMapping($this->objectClass);
     }
 
+    /**
+     * Get converted form label
+     *
+     * @param string $subject
+     * @return string
+     */
     public function convertSubjectToLabel($subject)
     {
         return 'form.label.'. $this->getObjectName() . '.' . $subject;
+    }
+
+    /**
+     * Get the current version of the entity
+     *
+     * @return object
+     */
+    public function getCurrent()
+    {
+        return $this->modContextProvider->getCurrentObject($this->getObjectName(), $this->getObjectId());
+    }
+
+    /**
+     * Get the attribute that uniquely identifies this entity
+     *
+     * @return string
+     */
+    protected function getIdentifierAttribute()
+    {
+        return $this->modContextProvider->getEntityIdentifier($this->getObjectName()) ? $this->modContextProvider->getEntityIdentifier($this->getObjectName()) : 'id';
+    }
+
+    protected function getCurrentAttributeValue($attribute)
+    {
+        return call_user_func(array($this->modContextProvider->getCurrentObject($this->getObjectName(), $this->getObjectId()), 'get' . $attribute));
+    }
+
+    public function getIdentifierOfCurrent()
+    {
+        return $this->getCurrentAttributeValue($this->getIdentifierAttribute());
     }
 }
--- src/Pwc/SirBundle/Service/ModContextProvider.php
+++ src/Pwc/SirBundle/Service/ModContextProvider.php
@@ -12,29 +12,189 @@ class ModContextProvider
 
     protected $reversedEntityMapping = array();
 
-    private $context = array();
+    private $glossary = array();
 
-    public $childObjects = array();
+    public $currentVersions = array();
 
     public function __construct(EntityManager $entityManager, array $entityMapping = array())
     {
         $this->entityMapping = $entityMapping;
         $this->entityManager = $entityManager;
 
-        foreach($entityMapping['entities'] as $entityFriendlyName => $entityConfig)
+        $this->setGlossary('vulnerability');
+        $this->setReversedMapping('vulnerability');
+    }
+
+    public function hasGlossary($entity, $attribute)
+    {
+        return array_key_exists($attribute, $this->glossary[$entity]) && isset($this->glossary[$entity][$attribute]) ? true : false;
+    }
+
+    public function getGlossary($entity, $attribute, $id)
+    {
+        return isset($this->glossary[$entity][$attribute][$id]) ? $this->glossary[$entity][$attribute][$id] : false;
+    }
+/*
+    public function getGlossary2()//$entity, $attribute, $id)
+    {
+        return $this->glossary;
+    }
+*/
+    protected function setGlossary($name)
+    {
+        $this->addGlossary($name);
+        foreach($this->getHierarchy($name) as $child) $this->addGlossary($child);
+/*
+        foreach($this->getEntities() as $entityFriendlyName => $entityConfig)
         {
             foreach($entityConfig['attributes'] as $attribute => $type)
             {
                 foreach($this->getForeignObjects($type) as $foreignObject)
                 {
-                    $this->context[$entityFriendlyName][$attribute][$foreignObject->getId()] = (string) $foreignObject;
+                    $this->glossary[$entityFriendlyName][$attribute][$foreignObject->getId()] = (string) $foreignObject;
                 }
-
-                $this->reversedEntityMapping[$type] = $attribute;
             }
-
-            $this->reversedEntityMapping[$entityConfig['namespace']] = $entityFriendlyName;
         }
+*/
+    }
+
+    protected function addGlossary($name)
+    {
+        foreach($this->getEntityAttributes($name) as $attribute => $namespace)
+        {
+            foreach($this->getForeignObjects($namespace) as $foreignObject)
+            {
+                $this->glossary[$name][$attribute][$foreignObject->getId()] = (string) $foreignObject;
+            }
+        }
+    }
+
+    protected function setReversedMapping($name)
+    {
+        $this->addReversedMapping($name);
+        foreach($this->getHierarchy($name) as $child) $this->addReversedMapping($child);
+    }
+
+    protected function addReversedMapping($name)
+    {
+        $this->reversedEntityMapping[$this->getEntityNamespace($name)] = $name;
+        foreach($this->getEntityAttributes($name) as $attribute => $namespace) $this->reversedEntityMapping[$namespace] = $attribute;
+    }
+
+    public function setCurrentVersions($name, $entity)
+    {
+        var_dump(get_class($entity));
+
+        var_dump($this->reversedEntityMapping);
+
+        $this->addCurrentVersions($name, array($entity));
+
+        foreach($this->getHierarchy($name) as $currentVersion)
+        {
+            $this->addCurrentVersions(
+                $currentVersion,
+                call_user_func(array($this->entityManager->getRepository($this->getEntityNamespace($currentVersion)), 'findBy' . $name), $entity)
+            );
+
+            //$this->addCurrentVersions($currentVersion, $em->getRepository($this->getEntityNamespace($currentVersion))->findByVulnerability($entity));
+        }
+    }
+
+    /**
+     * Get the complete entity mapping
+     *
+     * @return array
+     */
+    public function getEntityMapping()
+    {
+        return $this->entityMapping;
+    }
+
+    /**
+     * Get the complete list of hierarchies
+     *
+     * @return array
+     */
+    public function getHierarchies()
+    {
+        return $this->entityMapping['hierarchies'];
+    }
+
+    /**
+     * Get the complete list of entities
+     *
+     * @return array
+     */
+    public function getEntities()
+    {
+        return $this->entityMapping['entities'];
+    }
+
+    /**
+     * Get a hierarchy configuration
+     *
+     * @param string $hierarchy
+     * @return array
+     */
+    public function getHierarchy($hierarchy)
+    {
+        return array_key_exists($hierarchy, $this->entityMapping['hierarchies']) ? $this->entityMapping['hierarchies'][$hierarchy] : false;
+    }
+
+    /**
+     * Get a entity configuration
+
+     * @param string $entity
+     * @return array
+     */
+    public function getEntity($entity)
+    {
+        return array_key_exists($entity, $this->entityMapping['entities']) ? $this->entityMapping['entities'][$entity] : false;
+    }
+
+    /**
+     * Get a entity namespace
+
+     * @param string $entity
+     * @return string
+     */
+    public function getEntityNamespace($entity)
+    {
+        return array_key_exists($entity, $this->entityMapping['entities']) && isset($this->entityMapping['entities'][$entity]['namespace']) ? $this->entityMapping['entities'][$entity]['namespace'] : false;
+    }
+
+    /**
+     * Get a entity namespace
+
+     * @param string $entity
+     * @return array
+     */
+    public function getEntityAttributes($entity)
+    {
+        return array_key_exists($entity, $this->entityMapping['entities']) && isset($this->entityMapping['entities'][$entity]['attributes']) ? $this->entityMapping['entities'][$entity]['attributes'] : false;
+    }
+
+    /**
+     * Get an entity's attribute namespace
+
+     * @param string $entity
+     * @param string $attribute
+     * @return string
+     */
+    public function getAttributeNamespace($entity, $attribute)
+    {
+        return array_key_exists($entity, $this->entityMapping['entities']) && isset($this->entityMapping['entities'][$entity]['attributes'][$attribute]) ? $this->entityMapping['entities'][$entity]['attributes'][$attribute] : false;
+    }
+
+    /**
+     * Get a entity identifier
+
+     * @param string $entity
+     * @return string
+     */
+    public function getEntityIdentifier($entity)
+    {
+        return array_key_exists($entity, $this->entityMapping['entities']) && isset($this->entityMapping['entities'][$entity]['identifier']) ? $this->entityMapping['entities'][$entity]['identifier'] : false;
     }
 
     private function getForeignObjects($entity)
@@ -42,46 +202,34 @@ class ModContextProvider
         return $this->entityManager->getRepository($entity)->findAll();
     }
 
-    public function getContext()
+    public function getReversedEntityMapping($namespace)
     {
-        return $this->context;
+        return array_key_exists($namespace, $this->reversedEntityMapping) ? $this->reversedEntityMapping[$namespace] : false;
     }
 
-    public function getEntityMapping()
+    /**
+     * Add current versions of objects
+     *
+     * @param string $name
+     * @param array $currentVersions
+     */
+    public function addCurrentVersions($name, array $currentVersions)
     {
-        return $this->entityMapping;
-    }
-
-    public function getReversedEntityMapping()
-    {
-        return $this->reversedEntityMapping;
-    }
-
-    public function getHierarchies()
-    {
-        return $this->entityMapping['hierarchies'];
-    }
-
-    public function getChildren($parent)
-    {
-        return (array_key_exists($parent, $this->entityMapping['hierarchies']) ? $this->entityMapping['hierarchies'][$parent] : false);
-    }
-
-    public function getEntityNamespace($entity)
-    {
-        return (array_key_exists($entity, $this->entityMapping['entities']) ? $this->entityMapping['entities'][$entity]['namespace'] : false);
-    }
-
-    public function addChildObject($name, array $childObjects)
-    {
-        foreach($childObjects as $childObject)
+        foreach($currentVersions as $currentVersion)
         {
-            $this->childObjects[$name][$childObject->getId()] = $childObject;
+            $this->currentVersions[$name][$currentVersion->getId()] = $currentVersion;
         }
     }
 
-    public function getChildObject($name, $id)
+    /**
+     * Get current versions of objects
+     *
+     * @param string $name
+     * @param integer $id
+     * @return object
+     */
+    public function getCurrentObject($name, $id)
     {
-        return (array_key_exists($name, $this->childObjects) && array_key_exists($id, $this->childObjects[$name]) ? $this->childObjects[$name][$id] : false);
+        return (array_key_exists($name, $this->currentVersions) && array_key_exists($id, $this->currentVersions[$name]) ? $this->currentVersions[$name][$id] : false);
     }
 }
